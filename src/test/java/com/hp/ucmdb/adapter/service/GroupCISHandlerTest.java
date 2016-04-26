0b8c7e707c3b13959afc76ae7e0d5154ea533f69
Cisneros Barraza <benjamin.cisneros-barraza@hpe.com>
2016-04-26 12:21:16 -0500

    Refactor test to use paramaters instead

--- src/test/java/com/hp/ucmdb/adapter/service/GroupCISHandlerTest.java
+++ src/test/java/com/hp/ucmdb/adapter/service/GroupCISHandlerTest.java
@@ -9,7 +9,12 @@ import static org.mockito.Mockito.when;
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class GroupCISHandlerTest {
 
 	GroupCISHandler handler = new GroupCISHandler();
@@ -22,29 +27,10 @@ public class GroupCISHandlerTest {
 		when(request.getParameter(anyString())).thenReturn(null);
 		assertThat(handler.shouldGenerateDefaultBean(request), is(true));
 	}
-	
 	@Test
-	public void should_not_generate_default_bean_when_batch_id_parameter_is_not_null() throws Exception {
-		when(request.getParameter(GroupCISHandler.BATCH_ID)).thenReturn(ANY_STRING);
+	@Parameters({GroupCISHandler.BATCH_ID, GroupCISHandler.START_TIME, GroupCISHandler.END_TIME, GroupCISHandler.PAGE})
+	public void should_not_generate_default_bean_when_parameter_is_not_null(String parameter) throws Exception {
+		when(request.getParameter(parameter)).thenReturn(ANY_STRING);
 		assertThat(handler.shouldGenerateDefaultBean(request), is(false));
 	}
-	
-	@Test
-	public void should_not_generate_default_bean_when_start_time_parameter_is_not_null() throws Exception {
-		when(request.getParameter(GroupCISHandler.START_TIME)).thenReturn(ANY_STRING);
-		assertThat(handler.shouldGenerateDefaultBean(request), is(false));
-	}
-	
-	@Test
-	public void should_not_generate_default_bean_when_end_time_parameter_is_not_null() throws Exception {
-		when(request.getParameter(GroupCISHandler.END_TIME)).thenReturn(ANY_STRING);
-		assertThat(handler.shouldGenerateDefaultBean(request), is(false));
-	}
-	
-	@Test
-	public void should_not_generate_default_bean_when_page_paramater_is_not_null() throws Exception {
-		when(request.getParameter(GroupCISHandler.PAGE)).thenReturn(ANY_STRING);
-		assertThat(handler.shouldGenerateDefaultBean(request), is(false));
-	}
-
 }
