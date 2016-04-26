cf73f1c053946b0e9016977336092da6beb2a934
Cisneros Barraza <benjamin.cisneros-barraza@hpe.com>
2016-04-26 12:04:37 -0500

    Add new test for shouldGenerateDefaultBean(request) method

--- src/test/java/com/hp/ucmdb/adapter/service/GroupCISHandlerTest.java
+++ src/test/java/com/hp/ucmdb/adapter/service/GroupCISHandlerTest.java
@@ -1,7 +1,7 @@
 package com.hp.ucmdb.adapter.service;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -12,12 +12,21 @@ import org.junit.Test;
 
 public class GroupCISHandlerTest {
 
+	GroupCISHandler handler = new GroupCISHandler();
+	HttpServletRequest request = mock(HttpServletRequest.class);
+	
+	private static final String ANY_STRING = "any string";
+
 	@Test
 	public void should_generate_default_bean_when_request_parameters_are_null() {
-		GroupCISHandler handler = new GroupCISHandler();
-		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getParameter(anyString())).thenReturn(null);
 		assertThat(handler.shouldGenerateDefaultBean(request), is(true));
+	}
+	
+	@Test
+	public void should_not_generate_default_bean_when_batch_id_parameter_is_not_null() throws Exception {
+		when(request.getParameter(GroupCISHandler.BATCH_ID)).thenReturn(ANY_STRING);
+		assertThat(handler.shouldGenerateDefaultBean(request), is(false));
 	}
 
 }
