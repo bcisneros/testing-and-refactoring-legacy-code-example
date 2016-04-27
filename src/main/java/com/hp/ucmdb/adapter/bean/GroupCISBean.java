a6017a0f624db3b4fcd31d0ec45518ca650dc574
Cisneros Barraza <benjamin.cisneros-barraza@hpe.com>
2016-04-27 04:23:31 -0500

    Refactor getCountTitle() method and apply code reformat

--- src/main/java/com/hp/ucmdb/adapter/bean/GroupCISBean.java
+++ src/main/java/com/hp/ucmdb/adapter/bean/GroupCISBean.java
@@ -6,133 +6,137 @@ import java.util.ArrayList;
 
 public class GroupCISBean {
 
-    public static final int DEFAULT_INITIAL_PAGE_VALUE = 1;
-        private String id;
-    private String batchId;
-    private String startTime;
-    private String endTime;
-    private int page = DEFAULT_INITIAL_PAGE_VALUE;
-    private int endIndex;
-    private int totalCount = 0;
-    private ArrayList<CISGroupBean> groupBeans;
+	private static final String COUNT_TITLE_MESSAGE_FORMAT = "Showing %d to %d of %d";
+	private static final String NO_RECORDS_FOUND_MESSAGE = "Showing 0 of 0";
+	public static final int DEFAULT_INITIAL_PAGE_VALUE = 1;
+	private String id;
+	private String batchId;
+	private String startTime;
+	private String endTime;
+	private int page = DEFAULT_INITIAL_PAGE_VALUE;
+	private int endIndex;
+	private int totalCount = 0;
+	private ArrayList<CISGroupBean> groupBeans;
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public ArrayList<CISGroupBean> getGroupBeans() {
-        return groupBeans;
-    }
+	public ArrayList<CISGroupBean> getGroupBeans() {
+		return groupBeans;
+	}
 
-    public void setGroupBeans(ArrayList<CISGroupBean> groupBeans) {
-        this.groupBeans = groupBeans;
-    }
+	public void setGroupBeans(ArrayList<CISGroupBean> groupBeans) {
+		this.groupBeans = groupBeans;
+	}
 
-    public String getBatchId() {
-        return batchId;
-    }
+	public String getBatchId() {
+		return batchId;
+	}
 
-    public void setBatchId(String batchId) {
-        this.batchId = batchId;
-    }
+	public void setBatchId(String batchId) {
+		this.batchId = batchId;
+	}
 
-    /**
-     * @return the endIndex
-     */
-    public int getEndIndex() {
-        return endIndex;
-    }
+	/**
+	 * @return the endIndex
+	 */
+	public int getEndIndex() {
+		return endIndex;
+	}
 
-    /**
-     * @param endIndex the endIndex to set
-     */
-    public void setEndIndex(int endIndex) {
-        this.endIndex = endIndex;
-    }
+	/**
+	 * @param endIndex
+	 *            the endIndex to set
+	 */
+	public void setEndIndex(int endIndex) {
+		this.endIndex = endIndex;
+	}
 
-    /**
-     * @return the endTime
-     */
-    public String getEndTime() {
-        return endTime;
-    }
+	/**
+	 * @return the endTime
+	 */
+	public String getEndTime() {
+		return endTime;
+	}
 
-    /**
-     * @param endTime the endTime to set
-     */
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
+	/**
+	 * @param endTime
+	 *            the endTime to set
+	 */
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
 
-    /**
-     * @return the page
-     */
-    public int getPage() {
-        return page;
-    }
+	/**
+	 * @return the page
+	 */
+	public int getPage() {
+		return page;
+	}
 
-    /**
-     * @param page the page to set
-     */
-    public void setPage(int page) {
-        this.page = page;
-    }
+	/**
+	 * @param page
+	 *            the page to set
+	 */
+	public void setPage(int page) {
+		this.page = page;
+	}
 
-    /**
-     * @return the startIndex
-     */
-    public int getStartIndex() {
-        return (getPage() - 1) * PageHelper.PER_PAGE_COUNT + 1;
-    }
+	/**
+	 * @return the startIndex
+	 */
+	public int getStartIndex() {
+		return (getPage() - 1) * PageHelper.PER_PAGE_COUNT + 1;
+	}
 
-    /**
-     * @return the startTime
-     */
-    public String getStartTime() {
-        return startTime;
-    }
+	/**
+	 * @return the startTime
+	 */
+	public String getStartTime() {
+		return startTime;
+	}
 
-    /**
-     * @param startTime the startTime to set
-     */
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
+	/**
+	 * @param startTime
+	 *            the startTime to set
+	 */
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
 
-    /**
-     * @return the totalCount
-     */
-    public int getTotalCount() {
-        return totalCount;
-    }
+	/**
+	 * @return the totalCount
+	 */
+	public int getTotalCount() {
+		return totalCount;
+	}
 
-    /**
-     * @param totalCount the totalCount to set
-     */
-    public void setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
-    }
+	/**
+	 * @param totalCount
+	 *            the totalCount to set
+	 */
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
 
-    /**
-     * @return Generate String as Showing 1 to 20 of 2.
-     */
-    public String getCountTitle() {
-        StringBuilder output = new StringBuilder();
-        if (totalCount <= 0) {
-            output.append("Showing 0 of 0");
-        } else {
-            output.append("Showing ");
-            output.append(getStartIndex());
-            output.append(" to ");
-            output.append(endIndex);
-            output.append(" of ");
-            output.append(totalCount);
-        }
-
-        return output.toString();
-    }
+	/**
+	 * Retrieves the pager total message that informs the actual information
+	 * about what records are displayed on the results page
+	 * 
+	 * For example: "Showing 71 to 80 of 100" means that you are in the 8th page
+	 * and there are 100 records on the result list
+	 * 
+	 * When total count is 0 it displays "Showing 0 of 0" as default message
+	 * 
+	 * @return The count title String based on the total count number
+	 */
+	public String getCountTitle() {
+		return totalCount <= 0 ? NO_RECORDS_FOUND_MESSAGE
+				: String.format(COUNT_TITLE_MESSAGE_FORMAT, getStartIndex(), endIndex, totalCount);
+	}
 }
