8fcd83ceb22901bb6ce9ad3e4fee537899d4c17f
Cisneros Barraza <benjamin.cisneros-barraza@hpe.com>
2016-04-27 19:15:54 -0500

    Reformat code of GroupCISHandler

--- src/main/java/com/hp/ucmdb/adapter/service/GroupCISHandler.java
+++ src/main/java/com/hp/ucmdb/adapter/service/GroupCISHandler.java
@@ -8,60 +8,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.hp.ucmdb.adapter.bean.GroupCISBean;
 import com.hp.ucmdb.adapter.util.TimeHelper;
 
-
 public class GroupCISHandler {
 
-    public static final String PAGE = "page";
-    public static final String END_TIME = "endTime";
-    public static final String START_TIME = "startTime";
-    public static final String BATCH_ID = "batchId";
-    public static final String SEARCH_FILE = "searchFile";
-    public static final int BEFORE_TIME = 3;
-	
-    @Autowired
-    private TimeHelper timeHelper;
+	public static final String PAGE = "page";
+	public static final String END_TIME = "endTime";
+	public static final String START_TIME = "startTime";
+	public static final String BATCH_ID = "batchId";
+	public static final String SEARCH_FILE = "searchFile";
+	public static final int BEFORE_TIME = 3;
 
-    /**
-     */
-    public boolean shouldGenerateDefaultBean(HttpServletRequest request) {
-        return allParametersAreNull(request, BATCH_ID, START_TIME, END_TIME, PAGE);
-    }
+	@Autowired
+	private TimeHelper timeHelper;
 
+	/**
+	 */
+	public boolean shouldGenerateDefaultBean(HttpServletRequest request) {
+		return allParametersAreNull(request, BATCH_ID, START_TIME, END_TIME, PAGE);
+	}
 
-    /**
-     * @return Get the recent three days summary information.
-     */
-    public GroupCISBean handleDefaultParams() {
-        GroupCISBean bean = new GroupCISBean();
-        bean.setStartTime(timeHelper.dateBeforeTodayBy(GroupCISHandler.BEFORE_TIME));
-        bean.setEndTime(timeHelper.today());
-        bean.setPage(1);
-        return bean;
-    }
+	/**
+	 * @return Get the recent three days summary information.
+	 */
+	public GroupCISBean handleDefaultParams() {
+		GroupCISBean bean = new GroupCISBean();
+		bean.setStartTime(timeHelper.dateBeforeTodayBy(GroupCISHandler.BEFORE_TIME));
+		bean.setEndTime(timeHelper.today());
+		bean.setPage(1);
+		return bean;
+	}
 
-    /**
-     */
-    public GroupCISBean handleRequestParams(HttpServletRequest request) {
-        GroupCISBean bean = new GroupCISBean();
-        String batchId = request.getParameter(GroupCISHandler.BATCH_ID);
-        bean.setBatchId(StringUtils.isNotEmpty(batchId) ? batchId : "");
-        bean
-                .setStartTime(request
-                        .getParameter(GroupCISHandler.START_TIME));
-        bean.setEndTime(request.getParameter(GroupCISHandler.END_TIME));
+	/**
+	 */
+	public GroupCISBean handleRequestParams(HttpServletRequest request) {
+		GroupCISBean bean = new GroupCISBean();
+		String batchId = request.getParameter(GroupCISHandler.BATCH_ID);
+		bean.setBatchId(StringUtils.isNotEmpty(batchId) ? batchId : "");
+		bean.setStartTime(request.getParameter(GroupCISHandler.START_TIME));
+		bean.setEndTime(request.getParameter(GroupCISHandler.END_TIME));
 
-        String page = request.getParameter(GroupCISHandler.PAGE);
-        if (page != null && page != "") {
-            bean.setPage(Integer.parseInt(page));
-        }
-        return bean;
-    }
-    
-    private boolean allParametersAreNull(HttpServletRequest request, String... parameters) {
-    	for (String parameter : parameters) {
-			if (request.getParameter(parameter) != null) 
+		String page = request.getParameter(GroupCISHandler.PAGE);
+		if (page != null && page != "") {
+			bean.setPage(Integer.parseInt(page));
+		}
+		return bean;
+	}
+
+	private boolean allParametersAreNull(HttpServletRequest request, String... parameters) {
+		for (String parameter : parameters) {
+			if (request.getParameter(parameter) != null)
 				return false;
 		}
-    	return true;
-    }
+		return true;
+	}
 }
